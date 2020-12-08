58958f3436db50133abfb3b039e78b470e254832
Karl-Johan von Hacht <kjvonhacht@gmail.com>
2020-12-08 23:10:02 +0100

    Last commit for now

--- WhatOccupiesMyDisk/Class/DirectoriesFilesWithSizeInformation.cs
+++ WhatOccupiesMyDisk/Class/DirectoriesFilesWithSizeInformation.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
 namespace WhatOccupiesMyDisk.Class
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
     public class DirectoriesFilesWithSizeInformation : Dictionary<String, long>
     {
+        //private DirectoriesFilesWithSizeInformation _directoryWithSizeInformation;
+
         // redundant function
         public void AddPlain(String directory, long size)
         {
-            Add(directory, size);
+            //Add(directory, size);
+            AddToDirectoryWithSizeInformation(directory, size);
         }
 
         public void AddDictionary(Dictionary<String, long> source)
@@ -17,13 +22,40 @@ namespace WhatOccupiesMyDisk.Class
             if (source == null)
                 throw new ArgumentNullException(nameof(source));
             foreach (KeyValuePair<String, long> kvp in source)
-                Add(kvp.Key, kvp.Value);
+            {
+                //Add(kvp.Key, kvp.Value);
+                AddToDirectoryWithSizeInformation(kvp.Key, kvp.Value);
+            } 
         }
 
         public String GetRawString()
         {
             return String.Join(Environment.NewLine, this);
+            //return String.Join(Environment.NewLine, _directoryWithSizeInformation);
         }
 
+        private void AddToDirectoryWithSizeInformation(String path, long size)
+        {
+            String directoryName = DirectoryName(path);   
+
+            if (ContainsKey(directoryName))
+            {
+                this[directoryName] = this[directoryName] + size;
+            }
+            else
+            {
+                Add(directoryName, size);
+            }
+        }
+
+        private String DirectoryName(String path)
+        {
+            if (Directory.Exists(path))
+            {
+                return path;
+            }
+
+            return Path.GetDirectoryName(path);
+        }
     }
 }
